872def39eb9a599cf9dd3814f0881ba34a3df445
Akhil Gandu <akhilhello@gmail.com>
2020-11-04 14:06:26 -0800

    updated tests

--- routes/test.js
+++ routes/test.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 
-/*
-const pgp = require('pg-promise')(); 
-const db = pgp('postgres://akhil:apples@192.168.1.102:5432/uno'); 
-*/
-
 router.get("/", (request, response) => {
   db.any(
     `INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`
