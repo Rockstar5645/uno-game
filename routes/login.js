2262f757551c68b59b1b7107e42f2b472dd25aba
Akhil Gandu <akhilhello@gmail.com>
2020-11-18 15:38:48 -0800

    created login and signup UIs

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 // localhost/login/
 router.get('/', function (req, res, next) {
     res.render('login', { title: 'UNO' });
+});
+
+router.post('/', function(req, res, next) {
+    let username = req.body.username;
+    let password = req.body.password; 
+    console.log("username: " + username);
+    console.log("password: " + password); 
 });
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -6,4 +6,13 @@ router.get('/', function (req, res, next) {
     res.render('signup', { title: 'UNO' });
 });
 
+router.post('/', function(req, res, next) {
+    let username = req.body.username; 
+    let password = req.body.password; 
+    let confirmPassword = req.body.confirmPassword; 
+    let email = req.body.email; 
+    
+    console.log(username + ": " + password + ": " + confirmPassword + ": " + email); 
+});
+
 module.exports = router;
