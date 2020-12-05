1b555c80d2acd939e6a4b77f8f83195beeb1d857
Roberto Herman <roberto.herman021@gmail.com>
2020-12-04 23:34:27 -0800

    updated the games route

--- public/javascripts/game_stage.js
+++ public/javascripts/game_stage.js
@@ -1,25 +1,26 @@
-import {getCookie} from './util.js';
+import { getCookie } from './util.js';
 
-let socket = io(); 
+let socket = io();
 
 socket.emit('game-stage', {
-    user_id: getCookie('user_id'), 
+    // goes to services/game_stage.js
+    user_id: getCookie('user_id'),
     op: 'join'
-}); 
+});
 
 
 socket.on('room-joined', (msg) => {
-    console.log('room joined message', msg); 
-    let count = msg.player_count; 
+    console.log('room joined message', msg);
+    let { player_count, game_id, user_id } = msg;
 
     let elem = document.getElementById('status');
-    let players_left = 4 - count; 
+    let players_left = 4 - player_count;
 
     if (players_left === 0) {
 
-        elem.innerHTML = "Starting the game..."; 
-        window.location.href = "/games";
+        elem.innerHTML = "Starting the game...";
+        window.location.href = `/games/${game_id}/${user_id}`;
     } else {
-        elem.innerHTML = "Waiting for " + players_left + " more players."; 
+        elem.innerHTML = "Waiting for " + players_left + " more players.";
     }
 }); 
--- services/game_stage.js
+++ services/game_stage.js
@@ -1,31 +1,33 @@
 
-let { get_game_id } = require('../models/games.js'); 
-let { get_player_count } = require('../models/games.js'); 
+let { get_game_id } = require('../models/games.js');
+let { get_player_count } = require('../models/games.js');
 
 module.exports = async (msg, socket, io) => {
 
-    let user_id = msg.user_id; 
-    let op = msg.op; 
+    let user_id = msg.user_id;
+    let op = msg.op;
 
-    switch(op) {
-        case 'join': 
-            console.log('init operation called with user_id', user_id); 
-            let game_id = await get_game_id(user_id); 
-            let room_id = 'game-room-' + game_id; 
-            console.log('room id', room_id); 
+    switch (op) {
+        case 'join':
+            console.log('init operation called with user_id', user_id);
+            let game_id = await get_game_id(user_id);
+            let room_id = 'game-room-' + game_id;
+            console.log('room id', room_id);
 
             let player_count = await get_player_count(game_id);
-            console.log('player count', player_count); 
-            
-            socket.join(room_id); 
-            console.log(socket.rooms); 
+            console.log('player count', player_count);
+
+            socket.join(room_id);
+            console.log(socket.rooms);
             io.to(room_id).emit('room-joined', {
-                player_count: player_count
-            }); 
-            break; 
-      
+                player_count: player_count,
+                game_id: game_id,
+                user_id: user_id
+            });
+            break;
+
         default:
-            console.log('ollo'); 
-            break; 
-      }
+            console.log('ollo');
+            break;
+    }
 }
